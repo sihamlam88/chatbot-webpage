736f5a061dc955480163545a46384d29fc3edfc5
sihamlam88 <lamnouarsiham24@gmail.com>
2024-12-12 16:50:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'public' directory
+// Serve static files (HTML, CSS, JS) from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
